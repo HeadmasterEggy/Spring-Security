4b1492fd7c8dc475f9a3d95651843237ff5f4607
Joey <headmastereggy@gmail.com>
2024-02-01 10:35:13 +0800

    update

---
+++ spring-security07-mybatis-plus/src/main/java/com/example/Entity/Perm.java
@@ -0,0 +1,28 @@
+package com.example.Entity;
+
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@TableName("t_perm")
+@Data
+public class Perm {
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @TableField("name")
+    private String name;
+
+    @TableField("tag")
+    private String tag;
+
+    @TableField("user_id")
+    private Long userId;
+
+    @TableField(exist = false)
+    private User user;
+}
